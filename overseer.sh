e6aaf8fc0e672b32e5f5ff3e76253d66d027b6ff
Dustin Thomas <stdio@cptlobster.dev>
2025-08-09 16:54:14 -0400

    Improve overseer script

--- overseer.sh
+++ overseer.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
+
 function info {
     echo -e "[\e[1;36mINFO\e[0m overseer] $@"
 }
@@ -11,18 +13,34 @@ function err {
     echo -e "[\e[1;31mERR \e[0m overseer] $@"
 }
 
-info "Overseer program initiated."
+function fail {
+    err $@
+    exit 1
+}
+
+function check_if_exists {
+    CMD="$1"
+    if ! command -v $1 >/dev/null 2>&1
+    then
+        fail "Command $CMD is not present and is required to run."
+    fi
+}
+
+info "Overseer script initiated."
+
+info "Checking prerequisites..."
+
+# make sure all applications are present in the image before running
+check_if_exists "iocaine"
+check_if_exists "anubis"
+check_if_exists "openresty"
+check_if_exists "supervisord"
+check_if_exists "bene_gesserit"
+
 info "Generating configurations..."
 
-bene_gesserit
-
-info "Changing directory ownership..."
-
-chown -R nginx:arrakis /etc/nginx
-chown -R anubis:arrakis /etc/anubis
-chown -R iocaine:arrakis /etc/iocaine
+bene_gesserit || fail "Configuration script failed. See above logs for information."
 
 info "Starting processes using supervisord..."
 
-# TODO: start openresty/Anubis/Iocaine
-supervisord -c /etc/supervisord/supervisord.conf -n
+supervisord -c /etc/supervisord/supervisord.conf -n || fail "Supervisord failed to start. See above logs for information."
